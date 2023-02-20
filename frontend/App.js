b42f9c1e3f3527721ea5feb856dccd0cb056f7cd
Inamul07 <mohammedinamul87@gmail.com>
2023-02-20 19:41:35 +0530

    Change Project structure with backend

--- frontend/App.js
+++ frontend/App.js
@@ -15,7 +15,7 @@ const App = () => {
 	const [phone, setPhone] = useState("");
 	const [contacts, setContacts] = useState([]);
 
-	const domain = "http://192.168.29.45:8000/";
+	const domain = "http://192.168.137.137:8000/";
 
 	useEffect(() => {
 		loadData();
